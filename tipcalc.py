be1c0100964bb17a2dc6a14a38f00419ffdbe73d
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-10-06 08:32:20 +0300

    add tip calculator

---
+++ tipcalc.py
@@ -0,0 +1,10 @@
+total_bill = float(input("Enter total bill amount: "))
+tip_percentage = int(input("Enter percentage tip you wish to pay "))
+customers = int(input("Enter number of people to split bill: "))
+
+if tip_percentage not in (10,12,15) :
+    raise ValueError ("INVALID TIP: Tip percentage can only be 10,12 or 15")
+else:
+    payable_amount = (((tip_percentage/100) * total_bill) + total_bill) / customers #amount to be paid by each person
+
+print(f"Amount payable by each customer is {payable_amount}")
