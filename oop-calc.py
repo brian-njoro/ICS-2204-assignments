7540130422363a0066ef88c28befcf5daee961a3
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-11-05 18:26:26 +0300

    add methods to obtain operands and operations

--- oop-calc.py
+++ oop-calc.py
@@ -1,3 +1,13 @@
 class Calculator:
     def __init__(self, name):
         self.name = name
+
+    def get_operands(self):
+        # Prompt the user for the first operand
+        self.num1 = float(input("Enter the first operand: "))
+        # Prompt the user for the second operand
+        self.num2 = float(input("Enter the second operand: "))
+
+    def get_operation(self):
+        # Prompt the user for the operation to be carried out on operands
+        self.operation = input("Enter the operation ('+' for addition, '-' for subtraction, '*' for multiplication, '/' for division): ")
