db3ed886ba278847190d0ff455909ee01ad21faf
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-11-05 18:28:41 +0300

    define methods for each operation and result.

--- oop-calc.py
+++ oop-calc.py
@@ -11,3 +11,35 @@ class Calculator:
     def get_operation(self):
         # Prompt the user for the operation to be carried out on operands
         self.operation = input("Enter the operation ('+' for addition, '-' for subtraction, '*' for multiplication, '/' for division): ")
+
+    def perform_operation(self):
+        # Define functions for the numerical operations
+        def addition():
+            return self.num1 + self.num2
+
+        def subtract():
+            return self.num1 - self.num2
+
+        def multiply():
+            return self.num1 * self.num2
+
+        def division():
+            if self.num2 == 0:
+                raise ValueError("CANNOT DIVIDE BY ZERO!!")
+            return self.num1 / self.num2
+
+        # Conditional statement to perform the selected operation
+        if self.operation == "+":
+            self.result = addition()
+        elif self.operation == "-":
+            self.result = subtract()
+        elif self.operation == "*":
+            self.result = multiply()
+        elif self.operation == "/":
+            self.result = division()
+        else:
+            self.result = "Invalid operation selected."
+
+    def display_result(self):
+        # Print the result using an f-string
+        print(f"Hello, {self.name}, the result of your operation is: {self.result}")
